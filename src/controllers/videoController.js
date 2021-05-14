3c4f554800b699cf3d0f30d29ba0849756ba92fb
ystime25 <ystime25@gmail.com>
2021-05-14 20:50:51 +0900

    Intro to templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,34 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req,res) => {
-   return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "Hello",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        },
+        {
+            title: "Hello2",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 2
+        },
+        {
+            title: "Hello3",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 3
+        }
+];
+    return res.render("home", {pageTitle: "Home", videos});
 };
-export const edit = (req,res) => {
-    return res.send("Edit Video");
-};
+export const see = (req,res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req,res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req,res) => res.send("Search");
 export const upload = (req,res) => res.send("Upload");
 export const deleteVideo = (req,res) => {
