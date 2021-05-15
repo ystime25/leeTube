d52399ea14db8b10bb53745abe2dd55d4dc88b2f
ystime25 <ystime25@gmail.com>
2021-05-15 14:09:49 +0900

    Basic DB connections with MongoDB and Mongoose

---
+++ src/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb://127.0.0.1:27017/leetube", { 
+    useNewUrlParser: true, useUnifiedTopology: true 
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ DB Connection Confirmed ");
+const handleError = (error) => console.log("❌ DB Error Detected", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
