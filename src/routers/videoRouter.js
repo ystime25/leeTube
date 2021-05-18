7b0ac08981998cd50c2e4395ca6c0c53a316c80e
ystime25 <ystime25@gmail.com>
2021-05-18 15:41:56 +0900

    addtional url protection

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
+import { membersAccessMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.route("/:id([0-9a-f]{24})/edit").all(membersAccessMiddleware).get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").all(membersAccessMiddleware).get(deleteVideo);
+videoRouter.route("/upload").all(membersAccessMiddleware).get(getUpload).post(postUpload);
 
 export default videoRouter;
