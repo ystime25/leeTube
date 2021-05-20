57a490eac837101464792d6a6f0f54f0ada03127
ystime25 <ystime25@gmail.com>
2021-05-20 13:40:59 +0900

    webpack config for JS

---
+++ src/client/js/main.js
@@ -0,0 +1,1 @@
+alert(" hi I am webpack")
--- src/server.js
+++ src/server.js
@@ -25,7 +25,8 @@ app.use(
 );
 
 app.use(localsMiddleware);
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
