7803ace88135b80a85ae4feed63afd89b380c665
ystime25 <ystime25@gmail.com>
2021-05-24 14:26:09 +0900

    some problems with video btn

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,35 @@
-console.log("비디오 플레이어")
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = () => {
+    if (video.paused) {
+        video.play();
+    } else { 
+        video.pause();
+    }
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {
+    if (video.muted) {
+        video.muted = false;
+        muteBtn.innerText ="Mute";
+    } else {
+        video.muted = true;
+        muteBtn.innerText ="Unmute";
+    }
+};
+
+playBtn.addEventListener("click",handlePlayClick);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
+
+muteBtn.addEventListener("click",handleMute);
+
+
