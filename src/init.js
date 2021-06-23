09dff76276fef7eafd5ff52c4b02bad1d7489b27
ystime25 <ystime25@gmail.com>
2021-06-23 14:40:14 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListen = () =>
   console.log(`✅ Server Online on http://localhost:${PORT}`);
